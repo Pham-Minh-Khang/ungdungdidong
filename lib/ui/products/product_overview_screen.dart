d677d4a8fa16a09bf40cef5766083532425f27b0
PhamMinhKhangB1900242 <101611865+PhamMinhKhangB1900242@users.noreply.github.com>
2022-09-24 20:05:35 +0700

    Xay dung drawer va dinh tuyen theo ten

--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:myshop/ui/cart/cart_screen.dart';
 import '../shared/app_drawer.dart';
 import 'products_grid.dart';
+import '../cart/cart_manager.dart';
+import 'top_right_badge.dart';
 
 enum FilterOptions { favorites, all }
 
@@ -29,13 +31,16 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   }
 
   Widget buildShoppingCartIcon() {
-    return IconButton(
-      icon: const Icon(
-        Icons.shopping_cart,
+    return TopRightBadge(
+      child: IconButton(
+        icon: const Icon(
+          Icons.shopping_cart,
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(CartScreen.routeName);
+        },
       ),
-      onPressed: () {
-        Navigator.of(context).pushNamed(CartScreen.routeName);
-      },
+      data: CartManager().productCount,
     );
   }
 

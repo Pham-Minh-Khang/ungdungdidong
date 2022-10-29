5a6c71371cabc3e82295f25c87b5730069b26519
PhamMinhKhangB1900242 <101611865+PhamMinhKhangB1900242@users.noreply.github.com>
2022-10-29 06:14:02 -0700

    Luu san pham yeu thich len Realtime Database cua Firebase

--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/ui/products/products_manager.dart';
 import 'package:provider/provider.dart';
 import '../../models/product.dart';
 import '../cart/cart_manager.dart';
@@ -41,7 +42,7 @@ class ProductGridTile extends StatelessWidget {
             ),
             color: Theme.of(context).colorScheme.secondary,
             onPressed: () {
-              product.isFavorite = !isFavorite;
+              ctx.read<ProductsManager>().toggleFavoriteStatus(product);
             },
           );
         },
